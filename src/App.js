95f7c146040bf00593dc667314160d98fb71261b
Sylvie LeDuc <Sylvie@Sylvies-MacBook-Pro.local>
2019-11-06 00:12:30 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import './App.css';
 
 let shoppingCartItems = [
   {
-    item: 'War and Peace',
-    image: 'https://images4.penguinrandomhouse.com/cover/9781400079988',
-    description: 'War and Peace broadly focuses on Napoleon’s invasion of Russia in 1812 and follows three of the most well-known characters in literature',
+    item: 'Space Suit',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81xiC6NOdPL._SY450_.jpg',
+    description: 'This suit is extremely useful as it will permit travelers to breath.',
     price: 100
   },
+  {
+    item: 'Cucumber',
+    image: 'https://hosstools.com/wp-content/uploads/marketmore-cucumber-768x516.jpg' ,
+    description: 'This item is a tasty treat for your tastebuds.',
+    price: 100
+  },
+  {
+    item: 'Cotton Sweatsuit',
+    image: 'https://i.pinimg.com/originals/ae/07/7a/ae077ad7896ba93a2f317257ebc47af9.jpg',
+    description: 'For maximum comfort on your journey.',
+    price: 10
+  },
+  {
+    item: 'Cat',
+    image:'https://static.boredpanda.com/blog/wp-content/uploads/2018/04/5acb63d83493f__700-png.jpg',
+    description: 'A furry companion.',
+    price: 20
+  }
 ]
 
 function App() {
-  return (
+    return (
     <div className="App">
       <h1>Your Shopping Cart</h1>
      <SearchForm />
@@ -20,21 +38,30 @@ function App() {
   );
 }
 
+function formProduct(props) {
+
+  // console.log(props.item);
+  
+  return(
+    <div className="product">
+      <img src={props.image} width="100"></img>
+      <h1>{props.item}</h1>
+      <p>{props.description}</p>
+      <h3>{props.price}</h3> 
+    </div>
+  )
+}
+
 function ShoppingCart() {
+  const products = shoppingCartItems.map(formProduct);
   return(
     <div className="cart">
-      <Product />
+      {products}
+      
     </div>
   );
 }
 
-function Product() {
-  return(
-    <div className="product">
-      <img src={shoppingCartItems[0].image} width="100"/>
-    </div>
-  )
-}
 
 function SearchForm() {
   return(
