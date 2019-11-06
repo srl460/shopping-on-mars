08a9c9169c612352cb4ced04fe1bd2d55b436563
Sylvie LeDuc <Sylvie@Sylvies-MacBook-Pro.local>
2019-11-06 22:41:01 +0800

    initial comment

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ let shoppingCartItems = [
   {
     item: 'Space Suit',
     image: 'https://images-na.ssl-images-amazon.com/images/I/81xiC6NOdPL._SY450_.jpg',
-    description: 'This suit is extremely useful as it will permit travelers to breath.',
+    description: 'Will help you breath.',
     price: 100
   },
   {
     item: 'Cucumber',
     image: 'https://hosstools.com/wp-content/uploads/marketmore-cucumber-768x516.jpg' ,
-    description: 'This item is a tasty treat for your tastebuds.',
+    description: 'Health.',
     price: 100
   },
   {
     item: 'Cotton Sweatsuit',
     image: 'https://i.pinimg.com/originals/ae/07/7a/ae077ad7896ba93a2f317257ebc47af9.jpg',
-    description: 'For maximum comfort on your journey.',
+    description: 'For maximum comfort.',
     price: 10
   },
   {
     item: 'Cat',
     image:'https://static.boredpanda.com/blog/wp-content/uploads/2018/04/5acb63d83493f__700-png.jpg',
-    description: 'A furry companion.',
+    description: 'small cat',
     price: 20
   }
 ]
@@ -31,7 +31,7 @@ let shoppingCartItems = [
 function App() {
     return (
     <div className="App">
-      <h1>Your Shopping Cart</h1>
+      <h1>Heading to Mars</h1>
      <SearchForm />
      <ShoppingCart />
     </div>
@@ -45,9 +45,11 @@ function formProduct(props) {
   return(
     <div className="product">
       <img src={props.image} width="100"></img>
+      <div className="image">
       <h1>{props.item}</h1>
       <p>{props.description}</p>
       <h3>{props.price}</h3> 
+      </div>
     </div>
   )
 }
